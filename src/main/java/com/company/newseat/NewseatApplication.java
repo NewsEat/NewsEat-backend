28294a542cc7eb636f9bd4780732cb67559dde12
jiwonniy <jiwoni0707@gmail.com>
2025-07-24 19:56:40 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/company/newseat/NewseatApplication.java
@@ -0,0 +1,13 @@
+package com.company.newseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/newseat/NewseatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.newseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
