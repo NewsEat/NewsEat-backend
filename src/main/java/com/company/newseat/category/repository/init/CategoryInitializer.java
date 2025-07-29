f72d6992475f91670b551431fb73ee3959e2c61c
jiwonniy <jiwoni0707@gmail.com>
2025-07-29 16:00:51 +0900

    feat: #3 데이터 초기화 기능

---
+++ src/main/java/com/company/newseat/category/repository/init/CategoryInitializer.java
@@ -0,0 +1,75 @@
+package com.company.newseat.category.repository.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.company.newseat.category.domain.Category;
+import com.company.newseat.category.repository.CategoryRepository;
+import com.company.newseat.global.util.DataInit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+@RequiredArgsConstructor
+@Order(1)
+@DataInit
+public class CategoryInitializer implements ApplicationRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (categoryRepository.count() > 0) {
+            log.info("[Category] 카테고리 기본 데이터 존재");
+        } else {
+            List<Category> categoryList = new ArrayList<>();
+
+            Category CATEGORY_POLITICS = Category.builder()
+                    .name("정치")
+                    .orderIndex(1)
+                    .build();
+            Category CATEGORY_ECONOMY = Category.builder()
+                    .name("경제")
+                    .orderIndex(2)
+                    .build();
+            Category CATEGORY_SOCIETY = Category.builder()
+                    .name("사회")
+                    .orderIndex(3)
+                    .build();
+            Category CATEGORY_LIFESTYLE_CULTURE = Category.builder()
+                    .name("생활/문화")
+                    .orderIndex(4)
+                    .build();
+            Category CATEGORY_TECH_SCIENCE = Category.builder()
+                    .name("IT/과학")
+                    .orderIndex(5)
+                    .build();
+            Category CATEGORY_ENTERTAINMENT = Category.builder()
+                    .name("연예")
+                    .orderIndex(6)
+                    .build();
+            Category CATEGORY_SPORTS = Category.builder()
+                    .name("스포츠")
+                    .orderIndex(7)
+                    .build();
+            Category CATEGORY_WORLD = Category.builder()
+                    .name("세계")
+                    .orderIndex(8)
+                    .build();
+
+            categoryList.add(CATEGORY_POLITICS);
+            categoryList.add(CATEGORY_ECONOMY);
+            categoryList.add(CATEGORY_SOCIETY);
+            categoryList.add(CATEGORY_LIFESTYLE_CULTURE);
+            categoryList.add(CATEGORY_TECH_SCIENCE);
+            categoryList.add(CATEGORY_ENTERTAINMENT);
+            categoryList.add(CATEGORY_SPORTS);
+            categoryList.add(CATEGORY_WORLD);
+
+            categoryRepository.saveAll(categoryList);
+        }
+    }
+}
---
+++ src/main/java/com/company/newseat/user/repository/init/UserInitializer.java
@@ -0,0 +1,70 @@
+package com.company.newseat.user.repository.init;
+
+import com.company.newseat.global.util.DataInit;
+import com.company.newseat.user.domain.User;
+import com.company.newseat.user.domain.type.Provider;
+import com.company.newseat.user.domain.type.Role;
+import com.company.newseat.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@DataInit
+@Order(2)
+public class UserInitializer implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        if (userRepository.count() > 0) {
+            log.info("[User] 사용자 더미 데이터 존재");
+        } else {
+            List<User> memberList = new ArrayList<>();
+
+            User DUMMY_GUEST = User.builder()
+                    .email("guest@gmail.com")
+                    .password(passwordEncoder.encode("guest0000"))
+                    .provider(Provider.SELF)
+                    .role(Role.GUEST)
+                    .nickname("guest")
+                    .isDetox(false)
+                    .build();
+
+            User DUMMY_USER = User.builder()
+                    .email("user@gmail.com")
+                    .password(passwordEncoder.encode("user0000"))
+                    .provider(Provider.SELF)
+                    .role(Role.USER)
+                    .nickname("user")
+                    .isDetox(true)
+                    .build();
+
+            User DUMMY_ADMIN = User.builder()
+                    .email("admin@gmail.com")
+                    .password(passwordEncoder.encode("admin0000"))
+                    .provider(Provider.SELF)
+                    .role(Role.ADMIN)
+                    .nickname("admin")
+                    .isDetox(false)
+                    .build();
+
+            memberList.add(DUMMY_GUEST);
+            memberList.add(DUMMY_USER);
+            memberList.add(DUMMY_ADMIN);
+
+            userRepository.saveAll(memberList);
+        }
+    }
+}
