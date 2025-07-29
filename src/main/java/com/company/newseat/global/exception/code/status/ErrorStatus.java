2c93bf6cda5fb29eddc07992370d8bc256ec4508
jiwonniy <jiwoni0707@gmail.com>
2025-07-29 20:20:47 +0900

    feat: #3 error status 및 인증/인가 관련 핸들러 추가

--- src/main/java/com/company/newseat/global/exception/code/status/ErrorStatus.java
+++ src/main/java/com/company/newseat/global/exception/code/status/ErrorStatus.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
@@ -14,8 +17,22 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
+    // Auth
+    JWT_EXPIRED(UNAUTHORIZED, "AUTH4011", "토큰이 만료되었습니다."),
+    JWT_INVALID(UNAUTHORIZED, "AUTH4012", "유효하지 않거나 거부된 토큰입니다."),
+    LOGIN_FAILED_INCORRECT_PASSWORD(UNAUTHORIZED,"AUTH4013", "비밀번호가 일치하지 않습니다."),
+
+    ACCESS_DENIED(FORBIDDEN, "AUTH4031", "접근 권한이 없습니다."),
+
+    // User
+    USER_NICKNAME_REQUIRED(BAD_REQUEST, "USER4001", "닉네임은 필수 항목입니다."),
+    USER_EMAIL_DUPLICATED(BAD_REQUEST, "USER4002", "이미 존재하는 이메일입니다."),
+    USER_NOT_FOUND(NOT_FOUND, "USER4041", "사용자를 찾을 수 없습니다."),
+
+    // Category
+    CATEGORY_NOT_FOUND(NOT_FOUND, "CATEGORY4041", "카테고리 정보를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
@@ -37,7 +54,6 @@ public enum ErrorStatus implements BaseErrorCode {
                 .code(code)
                 .isSuccess(false)
                 .httpStatus(httpStatus)
-                .build()
-                ;
+                .build();
     }
 }
