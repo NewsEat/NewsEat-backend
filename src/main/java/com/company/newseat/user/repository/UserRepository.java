1a0b998d4dc70bfee470780497793fc8fa42d933
jiwonniy <jiwoni0707@gmail.com>
2025-07-29 20:49:10 +0900

    feat: #3 도메인에 컬럼 추가 및 관련 enum 타입, 레포지토리 쿼리 메서드 추가

--- src/main/java/com/company/newseat/user/repository/UserRepository.java
+++ src/main/java/com/company/newseat/user/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.company.newseat.user.repository;
 
 import com.company.newseat.user.domain.User;
+import com.company.newseat.user.domain.type.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmailAndProvider(String email, Provider provider);
 }
