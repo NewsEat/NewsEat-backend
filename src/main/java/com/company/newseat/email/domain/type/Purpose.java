c7093dde271a6ca6a6812a094b678e4879b50256
jiwonniy <jiwoni0707@gmail.com>
2025-08-05 20:55:28 +0900

    feat: #5 이메일 인증 관련 엔티티 및 enum 타입 추가

---
+++ src/main/java/com/company/newseat/email/domain/type/Purpose.java
@@ -0,0 +1,23 @@
+package com.company.newseat.email.domain.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Purpose {
+
+    NONE(0),
+    SIGNUP(1),
+    FIND_EMAIL(2),
+    FIND_PW(3);
+
+    private final int identifier;
+
+    public static Purpose of(int identifier) {
+        if (identifier == 1) return SIGNUP;
+        if (identifier == 2) return FIND_EMAIL;
+        if (identifier == 3) return FIND_PW;
+        return NONE;
+    }
+}
