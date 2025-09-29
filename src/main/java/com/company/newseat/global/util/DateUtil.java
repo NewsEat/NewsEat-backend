11db9d29f4d126701b9ae94549a45237f8093f60
jiwonniy <jiwoni0707@gmail.com>
2025-09-29 12:06:51 +0900

    feat: #38 DateTimeFormatter에 신규 포맷 추가

--- src/main/java/com/company/newseat/global/util/DateUtil.java
+++ src/main/java/com/company/newseat/global/util/DateUtil.java
@@ -8,15 +8,18 @@ public class DateUtil {
 
     private static final DateTimeFormatter[] INPUT_FORMATTERS = {
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"),
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"),
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSSSS"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd"),
             DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"),
             DateTimeFormatter.ofPattern("yyyy/MM/dd"),
             DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"),
-            DateTimeFormatter.ofPattern("yyyy.MM.dd")
+            DateTimeFormatter.ofPattern("yyyy.MM.dd"),
+            DateTimeFormatter.ISO_LOCAL_DATE_TIME,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS")
     };
 
-    private static final DateTimeFormatter OUTPUT_FORMATTERS =
-            DateTimeFormatter.ofPattern("yyyy.MM.dd");
+    private static final DateTimeFormatter OUTPUT_FORMATTERS = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
     private DateUtil() {
     }
